9723817feb421e048a0d1fbf87916aa7684dad60
kais-blkc <nagliyavarec@gmail.com>
2024-05-17 20:10:47 +0300

    Added adaptive for mobile 1000px > 320px

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,20 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet, RouterLink, MatSidenavModule, MatButtonModule],
+	imports: [
+		RouterOutlet,
+		RouterLink,
+		MatSidenavModule,
+		MatButtonModule,
+		MatIconModule,
+		MatToolbarModule,
+	],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
 })
